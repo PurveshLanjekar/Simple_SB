e6a156dcb05e70e69be47820c20f59629dc21e6e
Purvesh-Lanjekar <purveshlanjekar17@gmail.com>
2024-11-22 15:18:42 +0530

    first commit

---
+++ src/spp/basic.java
@@ -0,0 +1,20 @@
+package spp;
+
+public class basic {
+
+
+	public static void main(String[] args) {
+
+		System.out.println("Find Even number 0 to 50 ");
+
+		for (int i = 0; i <=50; i++) {
+
+			if (i%2==0) {
+				System.out.print(i+ " ");
+
+			}
+
+		}
+
+	}
+}
