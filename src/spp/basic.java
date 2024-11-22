9f6fc79c6cdd54031b2f8ec2cc9e67696627d2b2
Purvesh-Lanjekar <purveshlanjekar17@gmail.com>
2024-11-22 15:27:52 +0530

    first commit

--- src/spp/basic.java
+++ src/spp/basic.java
@@ -10,8 +10,10 @@ public class basic {
 		for (int i = 0; i <=50; i++) {
 
 			if (i%2==0) {
-				System.out.print(i+ " ");
+				System.out.print(i+"");
 
+			} else {
+				System.out.print(", ");
 			}
 
 		}
